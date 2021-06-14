de55fc50de92d0f09a5293854c9340a9631b9b40
Nikhil Jain <nikhiljain.lanet@gmail.com>
2021-06-14 11:35:46 +0530

    updated1

--- Backend/src/db/mongoose.js
+++ Backend/src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/handicraft-products', {
+mongoose.connect('mongodb+srv://nikhil:nikhil1999@cluster0.z30ix.mongodb.net/handicraftproject?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
 
